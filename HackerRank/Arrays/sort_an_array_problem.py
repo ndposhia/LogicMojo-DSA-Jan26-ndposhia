7847e5132c16f3d3e5f5daab6fcdb76e2e19165c
Nischay <130835479+ndposhia@users.noreply.github.com>
2026-01-21 13:29:24 +0530

    Update sort_an_array_problem.py

--- HackerRank/Arrays/sort_an_array_problem.py
+++ HackerRank/Arrays/sort_an_array_problem.py
@@ -16,7 +16,21 @@ import sys
 #
 
 def sort_an_array(n, arr):
-    arr.sort()
+    k = n-1
+    j=0
+    i=0
+    
+    while (j<=k):
+        if arr[j]==2:
+            arr[j], arr[k] = arr[k], arr[j]
+            k-=1
+        elif arr[j]==0:
+            arr[j], arr[i] = arr[i], arr[j]
+            j+=1
+            i+=1
+        else:
+            j+=1
+        
     return arr
 
 if __name__ == '__main__':
